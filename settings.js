862b8c6d924b7a5eaf91da1b44c3031bb4b9434c
Brightest2008 <brightonwuaha1@gmail.com>
2025-01-28 00:58:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,16 +3,16 @@
 You don't have to set this if you deploy using heroku because you can simply set them in environment variables, also don't forget to sleep */
 
 
-const session = process.env.SESSION || 'xxhyWTgT#aBR7sGS56HwoL6S8qhWi1vPIFpCmh8BFQ8bmjJoMlwU';
+const session = process.env.SESSION || 'V8ghEbgS#IwT35pTCWQfYw_54DErJRFCWJmW2YdpLdvxWL8f5IGo';
 
-const prefix = process.env.PREFIX || '';
+const prefix = process.env.PREFIX || '.';
 const mycode = process.env.CODE || "254";
-const author = process.env.STICKER_AUTHOR || 'Keith';
-const packname = process.env.PACKNAME || 'keith';
-const dev = process.env.DEV || '254796299159';
+const author = process.env.STICKER_AUTHOR || 'Bright';
+const packname = process.env.PACKNAME || 'Bright';
+const dev = process.env.DEV || '2349027090440';
 const DevKeith = dev.split(",");
-const botname = process.env.BOTNAME || 'KEITH-MD';
-const mode = process.env.MODE || 'public';
+const botname = process.env.BOTNAME || 'BRIGHT-MD';
+const mode = process.env.MODE || 'private';
 const gcpresence = process.env.GC_PRESENCE || 'false';
 const antionce = process.env.ANTIVIEWONCE || 'true';
 const sessionName = "session";
@@ -23,16 +23,16 @@ const api = process.env.API || 'true';
 const appname = process.env.APPNAME || 'true';
 const antispam = process.env.ANTISPAM || 'true';
 const chatbot = process.env.CHAT_BOT || 'true';
-const antilink = process.env.ANTILINK || 'true';
-const autoreact = process.env.AUTOREACT || 'true';
+const antilink = process.env.ANTILINK || 'false';
+const autoreact = process.env.AUTOREACT || 'false';
 const antibot = process.env.ANTIBOT || 'true';
 const anticall = process.env.ANTICALL || 'true';
 const antibad = process.env.ANTI_BAD_WORD || 'true';
 const antidelete = process.env.ANTIDELETE || 'true';
-const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
+const autoview = process.env.AUTOVIEW_STATUS || 'false';
+const autolike = process.env.AUTOLIKE_STATUS || 'false';
 const permit = process.env.PM_PERMIT || 'true';
-const autoread = process.env.AUTOREAD || 'true';
+const autoread = process.env.AUTOREAD || 'false';
 const autobio = process.env.AUTOBIO || 'false';
 
 module.exports = {
